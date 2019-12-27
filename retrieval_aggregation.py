7cc882cdab40e201a5994db9063c7322c4d1bc31
Heather Cronk <heather.cronk@colostate.edu>
2019-12-27 07:25:11 -0700

    Just loop over relevant SIDs and get data from those files. Consolodate to just use one HDF5 reader function.

--- retrieval_aggregation.py
+++ retrieval_aggregation.py
@@ -18,16 +18,33 @@ def read_hdf5_datafield_and_attrs(field, filename):
     """
     Extract data from an HDF5 file for a named field"
     """
-    
-    open_file = h5py.File(filename, 'r')
-    data_obj = open_file[field]
-    data = data_obj[:]
-    data_dtype = data.dtype
-    #attr_list = data_obj.attrs.items()
-    attr_dict = {}
-    for k, v in data_obj.attrs.items():
-        attr_dict[str(k)] = v[0]
-    
+    try:
+        open_file = h5py.File(filename, 'r')
+    except Exception as e:
+            print("Problem opening " + filename)
+            print("Error:", e)
+            sys.exit()
+    try:
+        data_obj = open_file[field]
+    except Exception as e:
+            print("Problem attaching to " + field)
+            print("Error:", e)
+            sys.exit()
+    try:
+        data = data_obj[:]
+    except Exception as e:
+            print("Problem extracting data from " + field)
+            print("Error:", e)
+            sys.exit()
+    try:
+        attr_dict = {}
+        for k, v in data_obj.attrs.items():
+            attr_dict[str(k)] = v[0]
+    except Exception as e:
+            print("Problem extracting attributes from " + field)
+            print("Error:", e)
+            sys.exit()
+
     return data, attr_dict
 
 def build_ds_list(name):
@@ -39,27 +56,29 @@ def aggregate(l1b_file):
     global DS_NAMES
     
     agg_file = re.sub("l1b_rx_intensity", "L2Ret", os.path.basename(l1b_file))
-    print(agg_file)
-    l1b_sid = get_hdf5_data("/SoundingGeometry/sounding_id", l1b_file)
-    print(l1b_sid)
-    #sys.exit()
+    l1b_sid, attr_dict = read_hdf5_datafield_and_attrs("/SoundingGeometry/sounding_id", l1b_file)
+    sid_bool = np.isin(l1b_sid, np.array(SEL_FILE_SIDS).astype("int64"))
+    relevant_sids = np.ma.masked_array(l1b_sid, ~sid_bool)
     ret_files = sorted(iglob(os.path.join(RET_DIR, "*L2FPRet*.h5")))
+    
     #Set up agg file
     open_file = h5py.File(ret_files[0], "r")
     DS_NAMES = []
     open_file.visit(build_ds_list)
     open_file.close()
     get_rid_of_groups = [DS_NAMES.remove(ds) for ds in DS_NAMES[:] if "/" not in ds]
-    
+    #Write agg file
     open_file = h5py.File(agg_file, "w") 
     for ds in DS_NAMES:
         print(ds)
         if "Dimensions/" in ds or "Shapes/" in ds:
             #Figure out at end?
+            #Eventually we may just want to put these fields in ourselves when we package the L2Dia/L2Std
             print("Skipping for now")
             continue
         elif "Metadata" in ds: 
             #go ahead and write this info in, it should be the same for all retrievals?
+            #Eventually we may just want to put these fields in ourselves when we package the L2Dia/L2Std
             data, attr_dict = read_hdf5_datafield_and_attrs(ds, ret_files[0])
             write_dataset = open_file.create_dataset(ds, data = data, dtype = data.dtype, compression="gzip")
             for a in attr_dict.keys():
@@ -80,13 +99,13 @@ def aggregate(l1b_file):
                 new_data_shape = list(data.shape)
                 new_data_shape[0] = len(l1b_sid)
             all_data = np.full(tuple(new_data_shape), np.nan, dtype = data_dtype)
-            for i, sid in enumerate(l1b_sid):
-                try:
-                    ret_file = glob(os.path.join(RET_DIR, "*L2FPRet_" + str(sid[0]) + "*.h5"))[0]
-                except IndexError:
-                    continue
-                get_data = get_hdf5_data(ds, ret_file)
-                all_data[i] = get_data
+            
+            for sid in relevant_sids.compressed():
+                #print(sid)
+                ret_file = glob(os.path.join(RET_DIR, "*L2FPRet_" + str(sid) + "*.h5"))[0]
+                xidx, yidx = np.where(l1b_sid == sid)
+                data, attr_dict = read_hdf5_datafield_and_attrs(ds, ret_file)
+                all_data[xidx] = data
                         
             write_dataset = open_file.create_dataset(ds, data = all_data, dtype = data_dtype, compression="gzip")
             for a in attr_dict.keys():
@@ -94,21 +113,6 @@ def aggregate(l1b_file):
                 attr_value = attr_dict.get(a)
                 write_dataset.attrs.create(a, data=attr_value)
     open_file.close()
-
-def get_hdf5_data(var, hdf5_file):
-    """
-    Extract given variable data from an HDF-5 file
-    """
-    f = h5py.File(hdf5_file, "r")
-    try: 
-        data = f[var][:]
-    except:
-        print("Problem retrieving " + var + " from " + hdf5_file)
-        f.close()
-        return
-    f.close()
-    return data
-
 
 if __name__ == "__main__":
     
